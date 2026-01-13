a737fa19f83b1f2c0b1fdde9f75b9bc60cfa7d69
Narayan <example@example.com>
2026-01-13 15:41:12 +0530

    updated SettingsScreen layout and typography behavior

--- composeApp/src/commonMain/kotlin/io/github/amanbutnot/moosic/data/constants/URLs.kt
+++ composeApp/src/commonMain/kotlin/io/github/amanbutnot/moosic/data/constants/URLs.kt
@@ -1,5 +1,5 @@
 package io.github.amanbutnot.moosic.data.constants
 
 fun getImage(id:String): String{
-
+val coverArtUrl = "https://music.hoelab.org/rest/getCoverArt.view?u=test&p=narayan7&v=1.16.1&c=feagegsag&f=json&id=1RsPD3I1gPmyloBgAOiSxe"
 }
